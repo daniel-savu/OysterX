f8fa435ab4ba766e557a698087db70d8ab32dc0e
alexandru197 <alexandru197@gmail.com>
2017-12-07 18:24:21 +0000

    Solved Date time 12AM/PM error and deprecated occurences

--- src/com/tfl/billing/Utility.java
+++ src/com/tfl/billing/Utility.java
@@ -1,11 +1,13 @@
 package com.tfl.billing;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Utility {
 
@@ -16,7 +18,7 @@ public class Utility {
     }
 
     public static BigDecimal roundToNearestPenny(BigDecimal poundsAndPence) {
-        return poundsAndPence.setScale(2, BigDecimal.ROUND_HALF_UP);
+        return poundsAndPence.setScale(2, RoundingMode.HALF_UP);
     }
 
     public static float stringTimeToFloatTime(String time) {
@@ -34,13 +36,15 @@ public class Utility {
     }
 
     public static float dateTimeToFloatTime(Date time) {
-        float hour = time.getHours();
-        float minutes = time.getMinutes();
+        Calendar calendar = GregorianCalendar.getInstance ();
+        calendar.setTime (time);
+        float hour = calendar.get(Calendar.HOUR_OF_DAY);
+        float minutes = calendar.get(Calendar.MINUTE);
         return (float) (hour + (minutes/60.0));
     }
 
     public static long dateFormatterToLong(String humanReadableTime) {
-        DateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+        DateFormat formatter = new SimpleDateFormat("HH:mm:ss");
         try {
             Date date = formatter.parse(humanReadableTime);
             return date.getTime();

9fe764a5621a45403bd7a6bad4adce737a0078ad
savudani8 <savudani04@yahoo.ro>
2017-12-07 18:30:14 +0000

    Fixes getTotal() test to fit custom hour

--- tests/com/tfl/billing/CustomerDecoratorTests.java
+++ tests/com/tfl/billing/CustomerDecoratorTests.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,38 +30,13 @@ public class CustomerDecoratorTests {
         return new Journey(journeyStart, journeyEnd);
     }
 
-
     @Test
     public void customerGetTotalIsCorrect() throws InterruptedException {
-        OysterCard myCard = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-
-        OysterCardReader paddingtonReader = OysterReaderLocator.atStation(Station.PADDINGTON);
-        OysterCardReader bakerStreetReader = OysterReaderLocator.atStation(Station.BAKER_STREET);
-        OysterCardReader kingsCrossReader = OysterReaderLocator.atStation(Station.KINGS_CROSS);
-
-        TravelTracker travelTracker = new TravelTracker();
-        travelTracker.connect(paddingtonReader, bakerStreetReader, kingsCrossReader);
-
-        paddingtonReader.touch(myCard);
-        bakerStreetReader.touch(myCard);
-        bakerStreetReader.touch(myCard);
-        kingsCrossReader.touch(myCard);
-
-        List<CustomerDecorator> customerDecorators = DatabaseController.getCustomerDecorators();
-        for (CustomerDecorator customerDecorator : customerDecorators) {
-            if(BigDecimalCompare.equalTo(customerDecorator.getTotal(), new BigDecimal("5.8"))) {
-                Assert.assertEquals(customerDecorator.fullName(), "Fred Bloggs");
-            }
-        }
-       //
-    }
-
-    @Test
-    public void customerGetTotalReturnValueIsCorrect() throws InterruptedException {
-        List<Journey> journeys = null;
+        ArrayList<Journey> journeys = new ArrayList<>();
         journeys.add(createTestJourneyWithStartTimeAndEndTime("12:00:00","12:10:00"));
         List<CustomerDecorator> customerDecorators = DatabaseController.getCustomerDecorators();
         CustomerDecorator customerDecorator = customerDecorators.get(0);
-        Assert.assertEquals(1.60,customerDecorator.getTotal(journeys));
+        BigDecimal expectedValue = new BigDecimal("1.6");
+        Assert.assertTrue(BigDecimalCompare.equalTo(expectedValue, customerDecorator.getTotal(journeys)));
     }
 }
